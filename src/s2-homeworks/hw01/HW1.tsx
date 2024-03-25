0f76ae95c3e8ac8a877408f42178cd8eed52b1ce
Catriiina <catherinaignatovich@gmail.com>
2024-03-25 22:08:28 +0100

    HW1

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -5,16 +5,19 @@ import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
 
-/*
-* 1 - описать тип MessageType
-* 2 - описать тип MessagePropsType в файле Message.tsx
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
-* 5 - сделать стили в соответствии с дизайном
-* */
+export type MessageType = {
+    id: number
+    user: UserType;
+    message: {
+        text: string;
+        time: string;
+    }
+}
 
-// нужно создать правильный тип вместо any
-export type MessageType = any
+export type UserType = {
+    avatar: string;
+    name: string;
+};
 
 // структуру объекта не менять
 export const message0: MessageType = {
