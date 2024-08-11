b299373eed38826777f893a7be81be3b90aadfc3
Catriiina <catherinaignatovich@gmail.com>
2024-08-11 19:53:22 +0200

    hw 03-4

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,23 @@
 import {UserType} from '../HW8'
-
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortedState = [...state];
+            sortedState.sort((a, b) => {
+                if (action.payload === 'up') {
+                    return a.name.localeCompare(b.name);
+                } else {
+                    return b.name.localeCompare(a.name);
+                }
+            });
+            return sortedState;
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload);
         }
         default:
             return state
