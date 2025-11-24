9f2f1d41891675bf59b70ddfbc1a9937e1fca6fb
Catriiina <catherinaignatovich@gmail.com>
2025-11-24 22:09:46 +0100

    HW4-02

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from './App.module.css'
 import HW5 from "../s2-homeworks/hw05/HW5";
-import HW9 from "../s2-homeworks/hw09/HW9";
+import HW10 from "../s2-homeworks/hw10/HW10";
 
 function App() {
     return (
@@ -13,7 +13,7 @@ function App() {
 
             {/*при выполнении дз 5 и более - закомментировать здесь дз 1-4, так как они есть внутри дз 5*/}
             <HW5/>
-            <HW9/>
+            <HW10/>
         </div>
     )
 }
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,7 @@
-import s from './Loader.module.css'
+import s from './Loader.module.css';
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => (
+    <svg className={s.loader} viewBox="0 0 50 50">
+        <circle className={s.circle} cx="25" cy="25" r="20" />
+    </svg>
+);
