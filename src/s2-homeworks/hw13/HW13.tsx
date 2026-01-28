c7bc524b82862a8f603c6e6d72f451705521b725
Catriiina <catherinaignatovich@gmail.com>
2026-01-28 20:14:18 +0100

    refactoring

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -32,7 +32,7 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText("...все ок))")
+                setText("...всё ок))")
                 setInfo(res.data.info)
                 setIsLoading(false)
 
